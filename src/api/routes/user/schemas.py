5ee522fbd895eed26193da5febee3f9f947dfd57
Eugene <toropkibn@gmail.com>
2024-03-12 10:43:17 +0300

    Update: user model, routes/user schemas, swagger

--- src/api/routes/user/schemas.py
+++ src/api/routes/user/schemas.py
@@ -7,7 +7,7 @@ class RegisterSchema(JsonSchema):
 	username = fields.Str(required=True,  validate=Length(min=1, max=30))
 	displayName = fields.Str(required=True,  validate=Length(min=1, max=30))
 	password = fields.Str(required=True, validate=Length(min=1))
-	email = fields.Str(required=True, validate=Length(min=1))
+	email = fields.Email(required=True, validate=Length(min=1))
 
 
 class LoginSchema(JsonSchema):
