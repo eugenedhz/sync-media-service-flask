fc85c84a507aed540604e1cd68cbbdaa237dfbe4
Eugene <you@example.com>
2024-03-19 21:00:32 +0300

    Update: json schemas configuration

--- src/api/schemas_config.py
+++ src/api/schemas_config.py
@@ -10,19 +10,21 @@ fields.Field.default_error_messages = {
 
 
 class Length(validate.Length):
-	def __init__(
-		self,
-		min: int | None = None,
-		max: int | None = None,
-		*,
-		equal: int | None = None,
-		error: str | None = None,
-	):
-
-		super().__init__(min=min, max=max, equal=equal, error=error)
-
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
 		self.error = 'INVALID_LENGTH'
 
+
+class Range(validate.Range):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.error = 'INVALID_RANGE'
+
+
+class Regexp(validate.Regexp):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.error = 'REGEXP_MISMATCH'
 
 
 class JsonSchema(Schema):
