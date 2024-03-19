b686b2e70931ba06399c13bf5484f2271e3ef088
Eugene <you@example.com>
2024-03-19 20:55:40 +0300

    Fix: pkg/query_params

--- pkg/query_params/select/parse.py
+++ pkg/query_params/select/parse.py
@@ -1,15 +1,14 @@
 from typing import Optional
 
 
-def parse_select(select_fields: Optional[str], valid_fields: tuple[str, ...]) -> Optional[tuple[str, ...]]:
-	if select_fields is None:
+def parse_select(select: Optional[str], valid_fields: tuple[str, ...]) -> Optional[tuple[str, ...]]:
+	if select is None:
 		return None
 
-	splitted_select_fields = select_fields.split(',')
-	
-	select_fields_set = set(splitted_select_fields)
-	valid_fields_set = set(valid_fields)
+	select = select.split(',')
 
-	filtered_select_fields = select_fields_set.intersection(valid_fields_set)
+	for field in select:
+		if field not in valid_fields:
+			raise KeyError
 
-	return (tuple(filtered_select_fields) if len(filtered_select_fields) != 0 else None)
+	return select
