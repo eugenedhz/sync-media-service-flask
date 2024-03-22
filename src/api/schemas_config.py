9dac2308b1e549e8bb410cb0b493245accc87994
Eugene <you@example.com>
2024-03-23 00:38:37 +0300

    Hotfix: minor fix identation api/schemas_config.py

--- src/api/schemas_config.py
+++ src/api/schemas_config.py
@@ -4,30 +4,29 @@ from src.api.error.custom_error import ApiError, ApiErrorInfo
 
 
 fields.Field.default_error_messages = {
-	        "required": "MISSING_FIELD",
-	        "null": "NOT_NULLABLE",
-	        "validator_failed": "INVALID_VALUE"
+	"required": "MISSING_FIELD",
+	"null": "NOT_NULLABLE",
+	"validator_failed": "INVALID_VALUE"
 }
 
 
 class JsonSchema(Schema):
+	error_messages = {
+        "unknown": "UNKNOWN_FIELD"
+	}
 
-		error_messages = {
-	        "unknown": "UNKNOWN_FIELD"
-    	}
+	def handle_error(self, exc, data, **kwargs):
+		invalid_fields = exc.messages
 
-		def handle_error(self, exc, data, **kwargs):
-			invalid_fields = exc.messages
+		for key in invalid_fields:
+			if 'Not a valid' in invalid_fields[key][0]:
+				invalid_fields[key] = 'INVALID_TYPE'
+			else:
+				invalid_fields[key] = invalid_fields[key][0]
 
-			for key in invalid_fields:
-				if 'Not a valid' in invalid_fields[key][0]:
-					invalid_fields[key] = 'INVALID_TYPE'
-				else:
-					invalid_fields[key] = invalid_fields[key][0]
-
-			raise ApiError(
-				ApiErrorInfo(
-					error_message = invalid_fields, 
-					description = "JSON you have sent failed validation."
-				)
+		raise ApiError(
+			ApiErrorInfo(
+				error_message = invalid_fields, 
+				description = "JSON you have sent failed validation."
 			)
+		)
