c4729a36c6ef644644d597b3fb6d57286269b837
okoroko <okorokov.alexei2014@gmail.com>
2024-03-26 12:02:22 +0300

    added create media

---
+++ src/repository/media/repo.py
@@ -0,0 +1,114 @@
+from sqlalchemy import Engine, update, select
+from sqlalchemy.orm import Session, defer
+from typing import Union, Optional, Any
+
+from src.domain.media import Media
+from src.interface.repository.media import MediaRepoInterface
+from src.repository.sqla_models.models import MediaModel
+from src.usecase.dto import QueryParametersDTO
+from src.usecase.media.dto import MediaUpdateDTO, MediaDTO, MediaCreateDTO
+
+from pkg.sqlalchemy.utils import get_first, get_all
+
+
+class MediaRepo(MediaRepoInterface):
+    def __init__(self, engine: Engine):
+        self.engine = engine
+
+    def store(self, media: Media) -> Media:
+        with Session(self.engine) as s:
+            new_media = MediaModel(**(media.to_dict()))
+
+            s.add(new_media)
+
+            s.commit()
+
+            s.refresh(new_media)
+
+        return Media(**new_media._asdict(Media))
+
+    # def get_by_id(self, id: int) -> User:
+    #     with Session(self.engine) as s:
+    #         query = (
+    #             select(UserModel)
+    #             .where(UserModel.id == id)
+    #         )
+    #
+    #         found_user = get_first(session=s, query=query)
+    #
+    #     if found_user is None:
+    #         return None
+    #
+    #     return User(**found_user._asdict(User))
+    #
+    # def get_by_username(self, username: str) -> User:
+    #     with Session(self.engine) as s:
+    #         query = (
+    #             select(UserModel)
+    #             .where(UserModel.username == username)
+    #         )
+    #
+    #         found_user = get_first(session=s, query=query)
+    #
+    #     if found_user is None:
+    #         return None
+    #
+    #     return User(**found_user._asdict(User))
+    #
+    # def update(self, id: int, update_user_dto: UserUpdateDTO) -> User:
+    #     with Session(self.engine) as s:
+    #         query = (
+    #             update(UserModel)
+    #             .where(UserModel.id == id)
+    #             .values(**update_user_dto)
+    #         )
+    #
+    #         s.execute(query)
+    #
+    #         s.commit()
+    #
+    #         updated_user = s.get(UserModel, id)
+    #
+    #     return User(**updated_user._asdict(User))
+    #
+    # def get_all(self, ids: Optional[tuple[int, ...]], query_parameters: QueryParametersDTO) -> list[UserDTO]:
+    #     with Session(self.engine) as s:
+    #         query = (
+    #             select(UserModel)
+    #             .options(defer(UserModel.passwordHash))
+    #         )
+    #
+    #         filters = query_parameters.filters
+    #
+    #         if ids is not None:
+    #             query = query.where(UserModel.id.in_(ids))
+    #
+    #         if filters is not None:
+    #             query = query.filter_by(**filters)
+    #
+    #         found_users = get_all(session=s, query=query)
+    #
+    #     found_users_dto = [UserDTO(**user._asdict(User)) for user in found_users]
+    #
+    #     return found_users_dto
+    #
+    # def delete(self, id: int) -> User:
+    #     with Session(self.engine) as s:
+    #         found_user = s.get(UserModel, id)
+    #
+    #         s.delete(found_user)
+    #
+    #         s.commit()
+    #
+    #     return User(**found_user._asdict(User))
+    #
+    def field_exists(self, field: dict[str: Any]) -> bool:
+        with Session(self.engine) as s:
+            query = (
+                select(MediaModel)
+                .filter_by(**field)
+            )
+
+            found_media = get_first(session=s, query=query)
+
+        return found_media is not None
---
+++ src/usecase/media/usecase.py
@@ -0,0 +1,77 @@
+from typing import Optional
+
+from src.domain.media import Media
+from src.interface.repository.media import MediaRepoInterface
+from src.usecase.dto import QueryParametersDTO
+from src.usecase.media.dto import (
+	MediaCreateDTO, MediaDTO, MediaUpdateDTO
+)
+
+
+class MediaUsecase():
+
+	def __init__(self, repo: MediaRepoInterface):
+		self.repo = repo
+
+	def create_media(self, media_dto: MediaCreateDTO) -> MediaDTO:
+
+		new_media = Media(
+			mName = media_dto.name,
+			description = media_dto.description,
+			thumbnail = media_dto.thumbnail,
+			preview = media_dto.preview,
+		)
+
+		stored_media = self.repo.store(new_media)
+		stored_media_dict = stored_media.to_dict()
+
+		return MediaDTO(**stored_media_dict)
+
+	def get_by_name(self, name: str) -> Optional[MediaDTO]:
+
+		found_media = self.repo.get_by_name(name=name)
+
+		if found_media is None:
+			return None
+
+		found_media_dict = found_media.to_dict()
+
+		return MediaDTO(**found_media_dict)
+
+	def get_by_id(self, id: int) -> Optional[MediaDTO]:
+
+		found_media = self.repo.get_by_id(id=id)
+
+		if found_media is None:
+			return None
+
+		found_media_dict = found_media.to_dict()
+
+		return MediaDTO(**found_media_dict)
+
+	def get_medias(self, ids: tuple[int, ...], query_parameters: QueryParametersDTO) -> list[MediaDTO]:
+		medias = self.repo.get_all(ids, query_parameters)
+
+		return medias
+
+	def update_media(self, id: int, update_media_dto: MediaUpdateDTO) -> MediaDTO:
+		updated_media = self.repo.update(id, update_media_dto)
+
+		updated_media_dict = updated_media.to_dict()
+
+		return MediaDTO(**updated_media_dict)
+
+	def delete_media(self, id: int) -> MediaDTO:
+		deleted_media = self.repo.delete(id=id)
+
+		deleted_media_dict = deleted_media.to_dict()
+
+		return MediaDTO(**deleted_media_dict)
+
+	def field_exists(self, name: str, value: str) -> bool:
+		field = dict()
+		field[name] = value
+
+		exists = self.repo.field_exists(field)
+
+		return exists
