b3df230a6861a8893514e3b749bf50760ba17c38
okoroko <okorokov.alexei2014@gmail.com>
2024-04-08 21:02:48 +0300

    Fix: create media schema fixes
    add length validation for name, description attributes
    moved thumbnail, preview attributes to separate schema
    set required argument to True where it was needed

--- src/api/routes/media/schemas.py
+++ src/api/routes/media/schemas.py
@@ -2,6 +2,8 @@ from marshmallow import fields
 
 from src.api.schemas_config import JsonSchema
 
+from pkg.json.validators import Length
+
 
 class MediaSchema(JsonSchema):
     id = fields.Int(required=False)
@@ -21,7 +23,10 @@ class UpdateMediaSchema(JsonSchema):
 
 
 class CreateMediaSchema(JsonSchema):
-    name = fields.Str(required=False)
-    description = fields.Str(required=False)
-    thumbnail = fields.Str(required=False)
-    preview = fields.Str(required=False)
+    name = fields.Str(required=True, validate=[Length(min=1, max=50)])
+    description = fields.Str(required=True, validate=[Length(max=200)])
+
+
+class MediaFilesSchema(JsonSchema):
+    thumbnail = fields.Field(required=True)
+    preview = fields.Field(required=True)
