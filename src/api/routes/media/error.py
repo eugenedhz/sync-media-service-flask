7e8ce4f0177e1bdccfbe8b205aef60a6cca9bddf
okoroko <okorokov.alexei2014@gmail.com>
2024-04-08 20:20:44 +0300

    Style: add spaces by the code style

--- src/api/routes/media/error.py
+++ src/api/routes/media/error.py
@@ -2,22 +2,22 @@ from src.api.error.custom_error import ApiErrorInfo
 
 MEDIA_API_ERRORS = {
     'MEDIA_EXISTS': ApiErrorInfo(
-        error_message='MEDIA_ALREADY_EXISTS',
-        status_code=409
+        error_message = 'MEDIA_ALREADY_EXISTS',
+        status_code = 409
     ),
 
     'MEDIA_NOT_FOUND': ApiErrorInfo(
-        error_message='MEDIA_NOT_FOUND',
-        status_code=404
+        error_message = 'MEDIA_NOT_FOUND',
+        status_code = 404
     ),
 
     'THUMBNAIL_NOT_PROVIDED': ApiErrorInfo(
-        error_message='THUMBNAIL_NOT_PROVIDED',
-        status_code=400
+        error_message = 'THUMBNAIL_NOT_PROVIDED',
+        status_code = 400
     ),
 
     'PREVIEW_NOT_PROVIDED': ApiErrorInfo(
-        error_message='PREVIEW_NOT_PROVIDED',
-        status_code=400
+        error_message = 'PREVIEW_NOT_PROVIDED',
+        status_code = 400
     ),
 }
