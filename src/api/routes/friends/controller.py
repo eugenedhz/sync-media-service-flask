ba1029e73021a05e69ac0a4901e1807ed86c4845
okoroko <okorokov.alexei2014@gmail.com>
2024-06-23 16:01:05 +0300

    Review fixes pt.4 (controller, usecase, repo)
    Fix: delete ids getting methods;
    Refactor: optimize db requests;
    Add: add is_already_friends, has_request db methods;

--- src/api/routes/friends/controller.py
+++ src/api/routes/friends/controller.py
@@ -10,6 +10,136 @@ from src.api.error.custom_error import ApiError
 from src.api.routes.user.schemas import UserSchema, UpdateUserSchema
 
 
+@app.route('/friends', methods=['POST'])
+@jwt_required()
+def add_friend():
+    request_params = request.args
+
+    user_id = get_jwt_identity()
+    friend_id = int(request_params.get('friend_id'))
+
+    if user_id == friend_id:
+        raise ApiError(FRIENDS_API_ERRORS['CANNOT_ADD_YOURSELF'])
+
+    is_user_exists = user_service.is_field_exists(name='id', value=friend_id)
+
+    if not is_user_exists:
+        raise ApiError(USER_API_ERRORS['USER_NOT_FOUND'])
+
+    sent_request = user_service.has_request(
+        requesting_user_id=user_id,
+        receiving_user_id=friend_id
+    )
+    is_already_friends = user_service.is_already_friends(
+        user_id=user_id,
+        friend_id=friend_id
+    )
+
+    if sent_request or is_already_friends:
+        raise ApiError(FRIENDS_API_ERRORS['ALREADY_REQUESTED'])
+
+    received_request = user_service.has_request(
+        requesting_user_id=friend_id,
+        receiving_user_id=user_id
+    )
+
+    if received_request:
+        added_friend = user_service.add_friend(
+            requesting_user_id=user_id,
+            receiving_user_id=friend_id,
+            received_request=received_request
+        )
+
+    else:
+        added_friend = user_service.send_friend_request(
+            requesting_user_id=user_id,
+            receiving_user_id=friend_id
+        )
+
+    return jsonify(added_friend._asdict())
+
+
+@app.route('/friends', methods=['DELETE'])
+@jwt_required()
+def delete_friend():
+    request_params = request.args
+
+    user_id = get_jwt_identity()
+    friend_id = int(request_params.get('friend_id'))
+
+    is_friend_exists = user_service.is_field_exists(name='id', value=friend_id)
+
+    if not is_friend_exists:
+        raise ApiError(USER_API_ERRORS['USER_NOT_FOUND'])
+
+    is_already_friends = user_service.is_already_friends(user_id=user_id, friend_id=friend_id)
+
+    if not is_already_friends:
+        raise ApiError(FRIENDS_API_ERRORS['FRIEND_NOT_FOUND'])
+
+    deleted_friend = user_service.delete_friend(user_id=user_id, friend_id=friend_id)
+
+    return jsonify(deleted_friend._asdict())
+
+
+@app.route('/friends/delete', methods=['DELETE'])
+@jwt_required()
+def delete_sent_friend_request():
+    request_params = request.args
+
+    user_id = get_jwt_identity()
+    friend_id = int(request_params.get('friend_id'))
+
+    is_friend_exists = user_service.is_field_exists(name='id', value=friend_id)
+
+    if not is_friend_exists:
+        raise ApiError(USER_API_ERRORS['USER_NOT_FOUND'])
+
+    sent_request = user_service.has_request(
+        requesting_user_id=user_id,
+        receiving_user_id=friend_id
+    )
+
+    if not sent_request:
+        raise ApiError(FRIENDS_API_ERRORS['REQUEST_NOT_FOUND'])
+
+    deleted_request_friend = user_service.delete_friend_request(
+        friend_id=friend_id,
+        request=sent_request
+    )
+
+    return jsonify(deleted_request_friend._asdict())
+
+
+@app.route('/friends/reject', methods=['DELETE'])
+@jwt_required()
+def delete_received_friend_request():
+    request_params = request.args
+
+    user_id = get_jwt_identity()
+    friend_id = int(request_params.get('friend_id'))
+
+    is_friend_exists = user_service.is_field_exists(name='id', value=friend_id)
+
+    if not is_friend_exists:
+        raise ApiError(USER_API_ERRORS['USER_NOT_FOUND'])
+
+    received_request = user_service.has_request(
+        requesting_user_id=friend_id,
+        receiving_user_id=user_id
+    )
+
+    if not received_request:
+        raise ApiError(FRIENDS_API_ERRORS['REQUEST_NOT_FOUND'])
+
+    deleted_request_friend = user_service.delete_friend_request(
+        friend_id=friend_id,
+        request=received_request
+    )
+
+    return jsonify(deleted_request_friend._asdict())
+
+
 @app.route('/friends', methods=['GET'])
 def get_user_friends():
     request_params = request.args
@@ -31,55 +161,6 @@ def get_user_friends():
     serialized_users = serialize_users(users)
 
     return jsonify(serialized_users)
-
-
-@app.route('/friends', methods=['POST'])
-@jwt_required()
-def add_friend():
-    request_params = request.args
-
-    user_id = get_jwt_identity()
-    friend_id = int(request_params.get('friend_id'))
-
-    if user_id == friend_id:
-        raise ApiError(FRIENDS_API_ERRORS['CANNOT_ADD_YOURSELF'])
-
-    is_user_exists = user_service.is_field_exists(name='id', value=friend_id)
-
-    if not is_user_exists:
-        raise ApiError(USER_API_ERRORS['USER_NOT_FOUND'])
-
-    already_requested_ids = user_service.get_already_requested_users_ids(user_id=user_id)
-
-    if friend_id in already_requested_ids:
-        raise ApiError(FRIENDS_API_ERRORS['ALREADY_REQUESTED'])
-
-    added_friend = user_service.add_friend(requesting_user_id=user_id, receiving_user_id=friend_id)
-
-    return jsonify(added_friend._asdict())
-
-
-@app.route('/friends', methods=['DELETE'])
-@jwt_required()
-def delete_friend():
-    request_params = request.args
-
-    user_id = get_jwt_identity()
-    friend_id = int(request_params.get('friend_id'))
-
-    is_friend_exists = user_service.is_field_exists(name='id', value=friend_id)
-
-    if not is_friend_exists:
-        raise ApiError(USER_API_ERRORS['USER_NOT_FOUND'])
-
-    user_friends_ids = user_service.get_friends_ids(user_id=user_id)
-
-    if friend_id not in user_friends_ids:
-        raise ApiError(FRIENDS_API_ERRORS['FRIEND_NOT_FOUND'])
-
-    deleted_friend = user_service.delete_friend(user_id=user_id, friend_id=friend_id)
-
-    return jsonify(deleted_friend._asdict())
 
 
 @app.route('/friends/received', methods=['GET'])
@@ -106,49 +187,3 @@ def get_sent_friend_requests():
     serialized_users = serialize_users(users)
 
     return jsonify(serialized_users)
-
-
-@app.route('/friends/delete', methods=['DELETE'])
-@jwt_required()
-def delete_sent_friend_request():
-    request_params = request.args
-
-    user_id = get_jwt_identity()
-    friend_id = int(request_params.get('friend_id'))
-
-    is_friend_exists = user_service.is_field_exists(name='id', value=friend_id)
-
-    if not is_friend_exists:
-        raise ApiError(USER_API_ERRORS['USER_NOT_FOUND'])
-
-    sent_request_ids = user_service.get_sent_requests_friends_ids(user_id=user_id)
-
-    if friend_id not in sent_request_ids:
-        raise ApiError(FRIENDS_API_ERRORS['REQUEST_NOT_FOUND'])
-
-    deleted_request_friend = user_service.delete_sent_friend_request(requesting_user_id=user_id, receiving_user_id=friend_id)
-
-    return jsonify(deleted_request_friend._asdict())
-
-
-@app.route('/friends/reject', methods=['DELETE'])
-@jwt_required()
-def delete_received_friend_request():
-    request_params = request.args
-
-    user_id = get_jwt_identity()
-    friend_id = int(request_params.get('friend_id'))
-
-    is_friend_exists = user_service.is_field_exists(name='id', value=friend_id)
-
-    if not is_friend_exists:
-        raise ApiError(USER_API_ERRORS['USER_NOT_FOUND'])
-
-    received_requests_ids = user_service.get_received_requests_friends_ids(user_id=user_id)
-
-    if friend_id not in received_requests_ids:
-        raise ApiError(FRIENDS_API_ERRORS['REQUEST_NOT_FOUND'])
-
-    deleted_request_friend = user_service.delete_received_friend_request(user_id=user_id, requesting_user_id=friend_id)
-
-    return jsonify(deleted_request_friend._asdict())
