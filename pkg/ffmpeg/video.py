db00aa18ee6ac84fbbdadad38900bdd4ad8c9e4d
Eugene <toropkibn@gmail.com>
2024-06-26 16:16:26 +0300

    fix(media): change max length for description field

--- pkg/ffmpeg/video.py
+++ pkg/ffmpeg/video.py
@@ -13,7 +13,7 @@ QUALITIES = {
 def transcode(input_path: str, output_path: str, quality: str) -> int:
 	command = f'ffmpeg -y -i { input_path } \
 	-preset veryslow -c:v libx264 -s { QUALITIES[quality] } \
-	-crf 20 -pix_fmt yuv420p -c:a copy \
+	-crf 20 -pix_fmt yuv420p -c:a aac \
 	-movflags +faststart { output_path }'
 
 	exit_code = call(command, shell=True)
