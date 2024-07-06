21fa0fccedf15f970547478b967ea682322aca05
Eugene <toropkibn@gmail.com>
2024-07-06 15:52:32 +0300

    hotfix(sockets): fix socket connection repo, video+player.controller

--- src/repository/socket_connection_session/repo.py
+++ src/repository/socket_connection_session/repo.py
@@ -12,22 +12,27 @@ class SocketConnectionSessionRepo(SocketConnectionSessionRepoInterface):
 		self.group = group
 		
 
-	def set(self, request_sid: str, user_id: int) -> None:
-		self.redis.hset(self.group, request_sid, user_id)
+	def set(self, key: str | int, value: int | str) -> None:
+		self.redis.hset(self.group, key, value)
 
 
-	def get(self, request_sid: str) -> Optional[int]:
-		user_id = self.redis.hget(self.group, request_sid)
-		if user_id is None:
+	def get(self, key: str | int) -> Optional[int | str]:
+		value = self.redis.hget(self.group, key)
+
+		if value is None:
 			return None
+		try:
+			user_id = int(value)
+			return user_id
+		except:
+			user_sid = value
+			return user_sid
 
-		return int(user_id)
+
+	def delete(self, key: str) -> None:
+		self.redis.hdel(self.group, key)
 
 
-	def delete(self, request_sid: str) -> None:
-		self.redis.hdel(self.group, request_sid)
-
-
-	def keys(self) -> tuple[str, ...]:
+	def keys(self) -> tuple[str | int, ...]:
 		keys = self.redis.hkeys(self.group)
 		return tuple(keys)
