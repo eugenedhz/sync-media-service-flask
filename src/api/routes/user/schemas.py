edcdefb4e8c07684d0462a5f9a3fb9c4dbc809c9
Eugene <toropkibn@gmail.com>
2024-07-10 09:06:02 +0300

    hotfix(sockets): fix get max order and get_by_order methods
    Additionally: fix schemas for fields.Int to fileds.Integer

--- src/api/routes/user/schemas.py
+++ src/api/routes/user/schemas.py
@@ -7,13 +7,13 @@ from pkg.json.validators import Length, Range, Regexp
 
 
 class UserSchema(JsonSchema):
-	id = fields.Int(required=False)
+	id = fields.Integer(required=False)
 	username = fields.Str(required=False)
-	registrationDate = fields.Int(required=False)
+	registrationDate = fields.Integer(required=False)
 	email = fields.Email(required=False)
 	displayName = fields.Str(required=False)
 	isBanned = fields.Boolean(required=False)
-	birthday = fields.Int(required=False)
+	birthday = fields.Integer(required=False)
 	description = fields.Str(required=False)
 	avatar = fields.Str(required=False)
 
@@ -22,6 +22,6 @@ class UpdateUserSchema(JsonSchema):
 	username = fields.Str(required=False, validate=[Length(min=5, max=30), Regexp(regex=Regex.USERNAME)])
 	email = fields.Email(required=False)
 	displayName = fields.Str(required=False, validate=Length(min=1, max=30))
-	birthday = fields.Int(required=False, validate=Range(min=86400))
+	birthday = fields.Integer(required=False, validate=Range(min=86400))
 	description = fields.Str(required=False, validate=Length(min=1, max=140))
 	password = fields.Str(required=False, validate=[Length(min=8), Regexp(regex=Regex.PASSWORD)])
--- src/api/sockets/video_player/schemas.py
+++ src/api/sockets/video_player/schemas.py
@@ -7,15 +7,15 @@ from pkg.json.validators import Range, Length
 
 class PlayerStateSyncWithUserSchema(JsonSchema):
 	userSID = fields.String(required=True, validate=Length(min=1))
-	currentTime = fields.Int(required=True, validate=Range(min=0))
+	currentTime = fields.Integer(required=True, validate=Range(min=0))
 	isPaused = fields.Boolean(required=True)
 
 
 class PlayerStateSyncWithEveryoneSchema(JsonSchema):
 	roomId = fields.Int(required=True, validate=Range(min=1))
-	currentTime = fields.Int(required=True, validate=Range(min=0))
+	currentTime = fields.Integer(required=True, validate=Range(min=0))
 	isPaused = fields.Boolean(required=True)
 
 
 class PlayerStateRequestSchema(JsonSchema):
-	roomId = fields.Int(required=True, validate=Range(min=1))
+	roomId = fields.Integer(required=True, validate=Range(min=1))
